b0c41690d57935a7ed1f8761dd2a4cd3dc33ce36
Łukasz Olejniczak <lolejniczak@google.com>
2022-08-14 19:48:31 +0200

    vertexai_dataset

--- backend/tests/jobs/unit/workers/bq_to_vertexai_dataset_exporter_tests.py
+++ backend/tests/jobs/unit/workers/bq_to_vertexai_dataset_exporter_tests.py
@@ -21,35 +21,25 @@ from google.api_core import operation
 
 from jobs.workers.bigquery import bq_to_vertexai_dataset
 
-# project
-from backend.jobs.workers.bigquery.bq_to_vertexai_dataset import BQToVertexAIDataset
-
 _TEST_PROJECT = "test-project"
 _TEST_LOCATION = "us-central1"
 
 # dataset
 _TEST_ID = "1028944691210842416"
 _TEST_DISPLAY_NAME = "my_dataset_1234"
-_TEST_DATA_LABEL_ITEMS = None
+_TEST_TABLE_NAME = "my_table_1234"
 _TEST_DESCRIPTION = "test description"
 
 # metadata_schema_uri
 _TEST_METADATA_SCHEMA_URI_TABULAR = schema.dataset.metadata.tabular
 _TEST_SOURCE_URI_BQ = "bq://my-project.my-dataset.table"
-_TEST_NAME = f"projects/{_TEST_PROJECT}/locations/{_TEST_LOCATION}/datasets/{_TEST_ID}"
+_TEST_RESOURCE_NAME = f"projects/{_TEST_PROJECT}/locations/{_TEST_LOCATION}/datasets/{_TEST_ID}"
 
 _TEST_METADATA_TABULAR_BQ = {
   "inputConfig": {"bigquerySource": {"uri": _TEST_SOURCE_URI_BQ}}
 }
 
 
-_TEST_DATASET_LIST = [
-  gca_dataset.Dataset(
-    name=_TEST_NAME,
-    display_name=_TEST_DISPLAY_NAME,
-    metadata_schema_uri=_TEST_METADATA_SCHEMA_URI_TABULAR,
-  )
-]
 
 
 
@@ -67,10 +57,20 @@ class BQToVertexAIDatasetExporterTest(parameterized.TestCase):
 
 
     @parameterized.parameters(
-      {'cfg_vertexai_dataset_name': 'test',
-     'cfg_clean_up': True,},
+      {
+        'cfg_vertexai_dataset_name': _TEST_DISPLAY_NAME,
+        'cfg_clean_up': True
+      },
+      {
+        'cfg_vertexai_dataset_name': _TEST_DISPLAY_NAME,
+        'cfg_clean_up': False
+      },
+      {
+        'cfg_vertexai_dataset_name': None,
+        'cfg_clean_up': True
+      },
     )
-    def test_create_veretxai_dataset_with_cleanup(self,
+    def test_create_veretxai_dataset(self,
          cfg_vertexai_dataset_name, cfg_clean_up):
       worker_inst = bq_to_vertexai_dataset.BQToVertexAIDataset(
         ##params
@@ -89,15 +89,25 @@ class BQToVertexAIDatasetExporterTest(parameterized.TestCase):
       )
 
 
+      if not cfg_vertexai_dataset_name:
+        display_name = f'{_TEST_PROJECT}.{_TEST_DISPLAY_NAME}.{_TEST_TABLE_NAME}'
+      else:
+        display_name = cfg_vertexai_dataset_name
+
+
       mock_datasets_client = mock.create_autospec(
          dataset_service_client.DatasetServiceClient, instance=True, spec_set=True)
 
-      mock_datasets_client.list_datasets.return_value = [gca_dataset.Dataset(
-        display_name=_TEST_DISPLAY_NAME,
+
+      mock_dataset = mock.Mock(gca_dataset.Dataset(
+        display_name=display_name,
         metadata_schema_uri=_TEST_METADATA_SCHEMA_URI_TABULAR,
         metadata=_TEST_METADATA_TABULAR_BQ,
-        name=_TEST_NAME
-      )]
+        name=display_name
+      ))
+
+
+      mock_datasets_client.list_datasets.return_value = [mock_dataset, mock_dataset]
 
       delete_dataset_lro_mock = mock.Mock(operation.Operation)
       delete_dataset_lro_mock.result.return_value = (
@@ -106,14 +116,8 @@ class BQToVertexAIDatasetExporterTest(parameterized.TestCase):
 
       mock_datasets_client.delete_dataset.return_value = delete_dataset_lro_mock
 
-      mock_tabular_dataset = mock.Mock(gca_dataset.Dataset(
-        display_name=_TEST_DISPLAY_NAME,
-        metadata_schema_uri=_TEST_METADATA_SCHEMA_URI_TABULAR,
-        metadata=_TEST_METADATA_TABULAR_BQ,
-        name=_TEST_NAME,
-      ))
-
-      mock_tabular_dataset.resource_name = _TEST_NAME
+      mock_tabular_dataset = mock_dataset
+      mock_tabular_dataset.resource_name = _TEST_RESOURCE_NAME
 
       mock_tabular_dataset.wait = mock.Mock(True)
 
@@ -132,7 +136,6 @@ class BQToVertexAIDatasetExporterTest(parameterized.TestCase):
           spec_set=True
         )
       )
-
 
       self.enter_context(
         mock.patch.object(
@@ -166,8 +169,9 @@ class BQToVertexAIDatasetExporterTest(parameterized.TestCase):
 
       worker_inst._execute()
       ## asserts
-      mock_datasets_client.list_datasets.assert_called_once()
-      ##mock_datasets_client.delete_dataset.assert_called_once()
+      if cfg_clean_up:
+         mock_datasets_client.list_datasets.assert_called_once()
+         mock_datasets_client.delete_dataset.assert_called_once()
       mocked_tablular_dataset_client.create.assert_called_once()
 
 
